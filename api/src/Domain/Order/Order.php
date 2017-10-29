c7acec35e2640ef935c946b09cd96dcb3c618040
Eric Braun <oqq@users.noreply.github.com>
2017-10-29 15:01:02 +0100

    updates order object, which needs products

--- api/src/Domain/Order/Order.php
+++ api/src/Domain/Order/Order.php
@@ -5,23 +5,29 @@ declare(strict_types=1);
 namespace Oqq\Broetchen\Domain\Order;
 
 use Assert\Assertion;
-use Oqq\Broetchen\Domain\Money;
+use Oqq\Broetchen\Domain\Product\ProductCollection;
+use Oqq\Broetchen\Domain\Service\ServiceId;
+use Oqq\Broetchen\Domain\User\UserId;
 
 final class Order
 {
     private $orderId;
+    private $userId;
+    private $serviceId;
     private $name;
-    private $price;
+    private $products;
 
     public static function fromArray(array $values): self
     {
-        Assertion::choicesNotEmpty($values, ['order_id', 'name', 'price']);
+        Assertion::choicesNotEmpty($values, ['order_id', 'user_id', 'service_id', 'name', 'products']);
 
+        $userId = UserId::fromString($values['user_id']);
+        $serviceId = ServiceId::fromString($values['service_id']);
         $orderId = OrderId::fromString($values['order_id']);
         $name = OrderName::fromString($values['name']);
-        $price = Money::fromString($values['price']);
+        $products = ProductCollection::fromArray($values['products']);
 
-        return new self($orderId, $name, $price);
+        return new self($orderId, $userId, $serviceId, $name, $products);
     }
 
     public function orderId(): OrderId
@@ -33,15 +39,24 @@ final class Order
     {
         return [
             'order_id' => $this->orderId->toString(),
+            'user_id' => $this->userId->toString(),
+            'service_id' => $this->serviceId->toString(),
             'name' => $this->name->toString(),
-            'price' => $this->price->toString(),
+            'products' => $this->products->getArrayCopy(),
         ];
     }
 
-    private function __construct(OrderId $orderId, OrderName $name, Money $price)
-    {
+    private function __construct(
+        OrderId $orderId,
+        UserId $userId,
+        ServiceId $serviceId,
+        OrderName $name,
+        ProductCollection $products
+    ) {
         $this->orderId = $orderId;
+        $this->userId = $userId;
+        $this->serviceId = $serviceId;
         $this->name = $name;
-        $this->price = $price;
+        $this->products = $products;
     }
 }
